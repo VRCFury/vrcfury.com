b69ac126cb2359b06e020900cac259a1fa8e835b
Senky <117542830+SenkyDragon@users.noreply.github.com>
2023-06-02 19:58:25 -0500

    docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'VRCFury',
+  tagline: 'Non-Destructive Tools for VRChat Avatars',
+  favicon: 'img/vrcf.png',
+
+  // Set the production url of your site here
+  url: 'https://vrcfury.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/VRCFury/vrcfury.com/',
+        },
+        theme: {
+          customCss: require.resolve('./custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      colorMode: {
+        defaultMode: 'dark',
+      },
+      navbar: {
+        title: 'VRCFury',
+        logo: {
+          alt: 'VRCFury Logo',
+          src: 'img/vrcf.png',
+          style: {
+            borderRadius: '5px',
+          }
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'docsSidebar',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/VRCFury/VRCFury',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} VRCFury Devs`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
