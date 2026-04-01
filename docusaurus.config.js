9d1f9bfd6ccce241bf6e2f24c1ae690f577a9120
Senky <117542830+SenkyDragon@users.noreply.github.com>
2026-03-31 21:18:13 -0500

    upgrade docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,14 @@ const config = {
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenAnchors: 'throw',
+  onDuplicateRoutes: 'throw',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+      onBrokenMarkdownImages: 'throw',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
